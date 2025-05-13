bf4aecbb84088f3d1d81d7471727e5468347c9c5
HuacongXie <solarsys@outlook.com>
2025-05-13 13:11:16 -0400

    Initial commit

---
+++ src/test/java/com/joycart/backend/JoycartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joycart.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoycartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

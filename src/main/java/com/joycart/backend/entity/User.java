302e72d9c705d55ff190338d0c2dc3f2ce11d191
HuacongXie <solarsys@outlook.com>
2025-05-13 14:15:29 -0400

    added entity User

---
+++ src/main/java/com/joycart/backend/entity/User.java
@@ -0,0 +1,62 @@
+package com.joycart.backend.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="jc_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    // if added another fields/attributes like age in V2 sql：
+    // @Column
+    // private Integer age;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
